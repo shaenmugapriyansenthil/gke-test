071b5c8cfd7150fc252f99905013087067aacea5
shaenmugapriyansenthil <78151523+shaenmugapriyansenthil@users.noreply.github.com>
2021-02-11 12:14:30 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,7 +4,7 @@ git clone https://github.com/shaenmugapriyansenthil/gke-test.git
 cd gke-test
 docker build -t quay.io/shaenmugapriyan_senthil/gke-test .
 cd ..
-docker login -u="shaenmugapriyan_senthil" -p="3P5FhiiDj9yTVSKu3UYF/3c8Xko65IK2wDXDa1S+kEEz4oKfKmBDX8bK2A+srGvr" quay.io
+docker login -u="shaenmugapriyan_senthil" -p="s2AT9mMA61MBMJDINVRqPPWrganAuppIHMzTucbIijV1cj4VwRRVuidoqxAqAdzv" quay.io
 docker push quay.io/shaenmugapriyan_senthil/gke-test
 rm -rf gke-test
 gcloud container clusters get-credentials my-k8s-cluster --zone us-central1-a --project gk-test-303111
